273ebc23cb1fc24a6d45e81d3a64b8bff4061377
Dinesh <dineshjangra0062@gmail.com>
2025-05-17 15:41:17 +0530

    student report

---
+++ tasks/studentportal.c
@@ -0,0 +1,120 @@
+#include <stdio.h>
+
+char menu();
+char registration();
+char display();
+
+int choice;
+char stid[5][20];
+char stname[5][50];
+char stnumber[5][15];
+char gmail[5][30];
+int stage[5];
+int stno = 0; 
+int uchoice;
+
+int main()
+{
+    printf("Welcome to the Student Management System.\n");
+    printf("If you want to use the system, type 1: ");
+    scanf("%d", &uchoice);
+
+    while (uchoice == 1)
+    {
+        menu(); // display menu
+        printf("Please enter your choice: ");
+        scanf("%d", &choice);
+
+        if (choice == 1)
+        {
+            registration();
+        } 
+        else if (choice == 2)
+        {
+            display();
+        }
+        else if (choice == 3)
+        {
+            printf("Exiting the program.\n");
+            break;
+        }
+        else
+        {
+            printf("Invalid choice. Please try again.\n");
+        }
+
+        printf("If you want to continue, type 1 (yes), or any other number (no): ");
+        scanf("%d", &uchoice);  // prompt again to continue or not
+    }
+
+    return 0;
+}
+
+char menu()
+{
+    printf("\n===== Menu =====\n");
+    printf("1. Student registration\n");
+    printf("2. Display student status\n");
+    printf("3. Exit\n");
+    return '0'; // '0' used to satisfy char return type, though unused
+}
+
+char registration() 
+{
+    int temp;
+    printf("\nNote: You can register up to 5 students in total.\n");
+    printf("Enter how many students to input: ");
+    scanf("%d", &temp);
+
+    if (stno + temp > 5)
+    {
+        printf("Error: You can only register %d more student(s).\n", 5 - stno);
+        return '0';
+    }
+
+    for (int i = 0; i < temp; i++) 
+    {
+        printf("\n--- Student %d ---\n", stno + 1);
+
+        printf("Enter the student ID: ");
+        scanf(" %[^\n]", stid[stno]);
+
+        printf("Enter the student name: ");
+        scanf(" %[^\n]", stname[stno]);
+
+        printf("Enter the student age: ");
+        scanf("%d", &stage[stno]);
+
+        printf("Enter the student contact number: ");
+        scanf(" %s", stnumber[stno]);
+
+        printf("Enter the student Gmail: ");
+        scanf(" %s", gmail[stno]);
+
+        stno++;
+    }
+
+    return '0';
+}
+
+char display()
+{
+    if (stno == 0)
+    {
+        printf("\nNo student data available to display.\n");
+        return '0';
+    }
+
+    printf("\n===== Student Information =====\n");
+    for (int i = 0; i < stno; i++)
+    {
+        printf("\n--- Student %d ---\n", i + 1);
+        printf("ID: %s\n", stid[i]);
+        printf("Name: %s\n", stname[i]);
+        printf("Age: %d\n", stage[i]);
+        printf("Contact: %s\n", stnumber[i]);
+        printf("Gmail: %s\n", gmail[i]);
+    }
+
+    return '0';
+}
